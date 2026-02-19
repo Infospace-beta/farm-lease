c1a43d4e92c3ee17cf0a1a73d8635acfed1f5dee
JEFFHONTEZ <jeffkamau8501@gmail.com>
2026-02-19 12:26:56 +0300

    😁😁😎😎

---
+++ frontend/src/app/(main)/dealer/layout.tsx
@@ -0,0 +1,3 @@
+export default function DealerLayout({ children }: { children: React.ReactNode }) {
+  return <section>{children}</section>;
+}
---
+++ frontend/src/app/(main)/lessee/layout.tsx
@@ -0,0 +1,3 @@
+export default function LesseeLayout({ children }: { children: React.ReactNode }) {
+  return <section>{children}</section>;
+}
