64481d455d981389740fc8ad440c9fe34fa7f30f
Martin-Ndirangu <martinnjoroge3266@gmail.com>
2026-02-20 11:12:52 +0300

    worked landmanagement

--- backend/farmlease/landmanagement/admin.py
+++ backend/farmlease/landmanagement/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import LandListing
 
-# Register your models here.
+@admin.register(LandListing)
+class LandListingAdmin(admin.ModelAdmin):
+    list_display = ('title', 'owner', 'is_verified', 'status', 'created_at')
+    list_filter = ('is_verified', 'status')
+    actions = ['verify_land']
+
+    @admin.action(description="Verify selected land listings")
+    def verify_land(self, request, queryset):
+        queryset.update(is_verified=True, status='Vacant')
+        self.message_user(request, "Selected lands are now live for Lessees.")
+
+
--- backend/farmlease/landmanagement/urls.py
+++ backend/farmlease/landmanagement/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('create-basic/', views.create_basic_info, name='create_basic'),
     path('<int:land_id>/add-soil/', views.add_soil_climate, name='add_soil'),
     path('<int:land_id>/upload-photos/', views.upload_land_images, name='upload_photos'),
+    path('browse/', views.browse_land, name='browse_land'),
     path('ownerdashboard/', views.LandownerDashboardStats.as_view(), name='owner'),
 ]
